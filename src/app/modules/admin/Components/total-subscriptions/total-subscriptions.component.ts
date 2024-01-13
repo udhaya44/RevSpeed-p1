9d38d7254da198104b97dd7c9bb4c5690b3cbe09
udhaya44 <60169135+udhaya44@users.noreply.github.com>
2024-01-13 22:44:01 +0530

    Revert "admin module"

--- src/app/modules/admin/Components/total-subscriptions/total-subscriptions.component.ts
+++ src/app/modules/admin/Components/total-subscriptions/total-subscriptions.component.ts
@@ -1,45 +1,10 @@
-// total-subscriptions.component.ts
-
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
-import { AdminService } from '../../admin.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-total-subscriptions',
   templateUrl: './total-subscriptions.component.html',
-  styleUrls: ['./total-subscriptions.component.scss']
+  styleUrl: './total-subscriptions.component.scss'
 })
-export class TotalSubscriptionsComponent implements OnInit {
-
-  displayedColumns: string[] = ['id', 'username', 'phone', 'email', 'userplanStatus', 'userplanid'];
-  dataSource: MatTableDataSource<any>;
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
-
-  constructor(
-    private adminService: AdminService,
-  ) {
-    this.dataSource = new MatTableDataSource<any>([]);
-  }
-
-  ngOnInit(): void {
-    this.loadSubscriptionData();
-  }
-
-  loadSubscriptionData() {
-    this.adminService.getActiveSubscriptions().subscribe(
-      (subscriptions: any[]) => {
-        this.dataSource = new MatTableDataSource(subscriptions);
-        this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort;
-      },
-      error => {
-        console.error('Error loading subscription data:', error);
-      }
-    );
-  }
+export class TotalSubscriptionsComponent {
 
 }
