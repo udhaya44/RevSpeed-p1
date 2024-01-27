5d26c8c1320333610e0dc46384cf3ec8c805ddd1
aakashsolanke99 <aakashsolanke99@gmail.com>
2024-01-27 23:41:23 +0530

    updated code

---
+++ src/app/Components/deletedialog/deletedialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deletedialog',
+  templateUrl: './deletedialog.component.html',
+  styleUrl: './deletedialog.component.scss'
+})
+export class DeletedialogComponent {
+
+}
---
+++ src/app/Components/resetdialog/resetdialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resetdialog',
+  templateUrl: './resetdialog.component.html',
+  styleUrl: './resetdialog.component.scss'
+})
+export class ResetdialogComponent {
+
+}
